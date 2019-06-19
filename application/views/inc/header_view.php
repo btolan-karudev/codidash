18081a2ea1ef91ba931cf06e33221c9034d4c729
Beniamin Tolan <beniamin.tolan@outlook.com>
2019-06-19 06:48:19 +0200

    home controller

--- application/views/inc/header_view.php
+++ application/views/inc/header_view.php
@@ -5,12 +5,14 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>test</title>
-    <link rel="stylesheet" href="<?=base_uri()?>public/css/bootstrap.min.css">
-    <link rel="stylesheet" href="<?=base_uri()?>public/css/style.css">
+    <title>CodiDash</title>
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="<?=base_url()?>public/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?=base_url()?>public/css/style.css">
 
-    <script src="<?=base_uri()?>public/js/jquery.js"></script>
-    <script src="<?=base_uri()?>public/js/bootstrap.js"></script>
+    <script src="<?=base_url()?>public/js/jquery.js"></script>
+    <script src="<?=base_url()?>public/js/bootstrap.js"></script>
 <body>
 
 <header>
