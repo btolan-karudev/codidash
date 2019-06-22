8a3ba3b7c793ba4178634f7f8922d24dcd23e745
Beniamin Tolan <beniamin.tolan@outlook.com>
2019-06-22 08:39:22 +0200

    form validation start

--- application/controllers/user.php
+++ application/controllers/user.php
@@ -31,6 +31,47 @@ class User extends CI_Controller
 
     }
 
+    public function register()
+    {
+        $this->output->set_content_type('application_json');
+
+        $this->form_validation->set_rules('login', 'Login', 'required|min_length[4]|max_length[16]|is_unique[user.login]');
+        $this->form_validation->set_rules('email', 'Email', 'required|valid_email|is_unique[user.email]');
+        $this->form_validation->set_rules('password', 'Password', 'required|min_length[4]|max_length[16]|matches[confirm_password]');
+
+        if ($this->form_validation->run() == false) {
+            $this->output->set_output(json_encode([
+                'result' => 0,
+                'data' => validation_errors()
+            ]));
+            return false;
+        }
+
+        $login = $this->input->post('login');
+        $email = $this->input->post('email');
+        $password = $this->input->post('password');
+        $confirm_password = $this->input->post('confirm_password');
+
+        $user_id = $this->user_model->insert([
+            'login' => $login,
+            'email' => $email,
+            'password' => hash('sha256', $password . SALT)
+        ]);
+
+        echo $user_id;
+        die('not yet ready');
+
+        $this->output->set_content_type('application_json');
+
+        if ($result) {
+            $this->session->set_userdata(['user_id' => $result[0]['user_id']]);
+            $this->output->set_output(json_encode(['result' => 1]));
+            return false;
+        }
+
+
+    }
+
     public function test_get()
     {
         $data = $this->user_model->get(9);
